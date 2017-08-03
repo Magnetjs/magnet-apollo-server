60af820b264e7de04b392f654dd956ac30458386
Kieve Chua <kievechua@gmail.com>
2017-08-03 14:21:16 +0800

    Fix old file name

--- dist/resolvers.js
+++ dist/resolvers.js
@@ -7,20 +7,19 @@ const map = require("lodash/fp/map");
 const mapValues = require("lodash/fp/mapValues");
 const values = require("lodash/fp/values");
 const compact = require("lodash/fp/compact");
+const isEmpty = require("lodash/isEmpty");
 function default_1(resolversDir, app) {
-    const domains = requireAll(path.join(app.config.baseDirPath, resolversDir));
     const getMainFunction = (pSchemas, field) => flow(mapValues(field), values, compact)(pSchemas);
     const getResolver = (pSchemas, field) => flow(map((resolver) => resolver(app)), map(field), values, compact)(pSchemas);
+    const domains = requireAll(path.join(app.config.baseDirPath, resolversDir));
     const defaults = getMainFunction(domains, 'default');
-    const resolveFunctions = {
-        Query: Object.assign({}, ...getResolver(defaults, 'Query')),
-        Mutation: Object.assign({}, ...getResolver(defaults, 'Mutation'))
-        // Subscription: {
-        //   postUpvoted (post) {
-        //     return post
-        //   }
-        // }
-    };
+    const resolveFunctions = {};
+    for (const property of ['Mutation', 'Query', 'Subscription']) {
+        const resolvers = Object.assign({}, ...getResolver(defaults, property));
+        if (!isEmpty(resolvers)) {
+            resolveFunctions[property] = resolvers;
+        }
+    }
     return Object.assign(resolveFunctions, ...getResolver(defaults, 'Root'));
 }
 exports.default = default_1;
