b31a62a026db542541be51e54543da165bd010b9
Kieve Chua <kievechua@gmail.com>
2017-08-23 10:10:42 +0800

    Fix error doesn't get pass over

--- dist/koa.js
+++ dist/koa.js
@@ -23,27 +23,7 @@ class MagnetGraphqlServerKoa extends module_1.Module {
     }
     setup() {
         return __awaiter(this, void 0, void 0, function* () {
-            const formatError = graphql_apollo_errors_1.formatErrorGenerator({
-                // logger: this.log,
-                publicDataPath: 'public',
-                showLocations: !this.app.config.env.prod,
-                showPath: !this.app.config.env.prod,
-                hideSensitiveData: this.app.config.env.prod,
-                hooks: {
-                    // This run on the error you really throw from your code (not the graphql error - it means not with path and locations)
-                    onOriginalError: (originalError) => { this.log.error(originalError.message); },
-                    // This will run on the processed error, which means after we convert it to boom error if needed
-                    // and after we added the path and location (if requested)
-                    // If the error is not a boom error, this error won't include the original message but general internal server error message
-                    // This will run before we take only the payload and the public path of data
-                    onProcessedError: (processedError) => { this.log.error(processedError.message); },
-                    // This will run on the final error, it will only contains the output.payload, and if you configured the publicDataPath
-                    // it will only contain this data under the data object
-                    // If the error is internal error this error will be a wrapped internal error which not contains the sensitive details
-                    // This is the error which will be sent to the client
-                    onFinalError: (finalError) => { this.log.error(finalError.message); }
-                }
-            });
+            const formatError = graphql_apollo_errors_1.formatErrorGenerator(this.config.error);
             const typeDefs = typeDefs_1.default(path.join(this.app.config.baseDirPath, this.config.schemasDir));
             if (!typeDefs.length) {
                 this.log.error('No schema definition');
